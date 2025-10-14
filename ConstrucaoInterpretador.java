46f187942b84757cdc41586d69b37cfd8e7ed9fd
Kleiton Linneker <134451444+kleitonlinneker@users.noreply.github.com>
2025-10-14 15:09:48 -0300

    Update ConstrucaoInterpretador.java

--- ConstrucaoInterpretador.java
+++ ConstrucaoInterpretador.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste Compiladores !");
+        System.out.println("Construindo um interpretador!");
     }
 }
