adcf6ab5ae29a4acbafc9cecfe62cbb0fa7ac02a
kleitonlinneker <kleiton.linneker@discente.ufma.br>
2025-10-15 00:44:19 -0300

    Sinc Github / Intellij concluida

--- Parser.java
+++ Parser.java
@@ -7,7 +7,53 @@ public class Parser {
     }
 
     public void parse () {
-        // aqui ainda ira o código
+        expr();
     }
+
+    private char peek () {
+        if (current < input.length)
+            return (char)input[current];
+        return '\0';
+    }
+
+    private void match (char c) {
+        if (c == peek()) {
+            current++;
+        } else {
+            throw new Error("syntax error");
+        }
+    }
+
+    void expr() {
+        digit();
+        oper();
+    }
+
+    void digit () {
+        if (Character.isDigit(peek())) {
+            System.out.println("push " + peek());
+            match(peek());
+        } else {
+            throw new Error("syntax error");
+        }
+    }
+
+
+
+    void oper () {
+        if (peek() == '+') {
+            match('+');
+            digit();
+            System.out.println("add");
+            oper();
+        } else if (peek() == '-') {
+            match('-');
+            digit();
+            System.out.println("sub");
+            oper();
+        }
+    }
+
+
 
 }
