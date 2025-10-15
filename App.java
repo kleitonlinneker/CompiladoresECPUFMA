fb88452a247bb0343e852be76e6b6cb885cfcae5
kleitonlinneker <kleiton.linneker@discente.ufma.br>
2025-10-14 23:50:01 -0300

    Sinc Github / Intellij concluida

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Sincronização efetuada com sucesso);
+        System.out.println("Sincronização efetuada com sucesso");
     }
 }
